7c8c51cc66939c9f2bbd7e0316e8b265b1927260
Mostafa Mohamed <116523896+mostafamohamed4@users.noreply.github.com>
2023-05-13 22:22:42 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-import { createBrowserRouter, createHashRouter, RouterProvider, useNavigate } from 'react-router-dom'
+import { createBrowserRouter, createHashRouter, HashRouter, RouterProvider, useNavigate } from 'react-router-dom'
 import Layout from './Components/Layout/Layout';
 import Carts from './Components/Carts/Carts';
 import Shopping from './Components/Shopping/Shopping';
 import { CartContextProvider } from './Context/Carts';
 
 function App() {
-  let routers = createBrowserRouter([
+  let routers = createHashRouter([
     {
       path: '', element: <Layout />, children: [
 
--- src/Components/Shopping/Shopping.jsx
+++ src/Components/Shopping/Shopping.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios'
 import React, { useContext, useEffect, useState } from 'react'
-// import OwlCarousel from 'react-owl-carousel';
+import OwlCarousel from 'react-owl-carousel';
 import $ from "jquery";
 import './Shopping.css'
 import SimilarProducts from '../SimilarProducts/SimilarProducts';
 import Carts from '../Carts/Carts';
 import { cartcontext } from '../../Context/Carts';
-// import 'owl.carousel/dist/assets/owl.carousel.css';
-// import 'owl.carousel/dist/assets/owl.theme.default.css';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 export default function Shopping() {
     let { addcart, increaseAmount, updatedowen, updateup, quantity } = useContext(cartcontext);
 
@@ -41,21 +41,17 @@ export default function Shopping() {
                     <div className='container'>
                         <div className='row  pt-5 g-3'>
                             <div className='col-lg-5 col-md-10 m-auto' >
-                                {/* <div className='iamges text-center'>
+                                <div className='iamges text-center'>
 
-                            <OwlCarousel className='owl-theme' items={1} loop>
-                                {listprodectDetials.iImagessmages?.map((element, index) => {
-                                    return <>
+                                    <OwlCarousel className='owl-theme' items={1} loop>
+                                                <div className='iamges'   >
+                                                    <img src={listprodectDetials.image} className='w-100'  alt="" />
+                                                </div>
+                                           
 
-                                        <div className='iamges' key={index}  >
-                                            <img src={element} className='w-100' height={555} alt="" />
-                                        </div>
-                                    </>
-                                })
-
-                                }          </OwlCarousel>
-                        </div> */}
-                                <div className='w-100 mx-auto' data-bs-ride="carousel">
+                                            </OwlCarousel>
+                                </div>
+                                {/* <div className='w-100 mx-auto' data-bs-ride="carousel">
                                     {imageCover == null ? <img src={listprodectDetials.image} className='w-100' height={530} alt="" /> : <img src={imageCover} className='w-75' height={480} alt="" />
                                     }
                                     <div className='d-flex'>
@@ -64,10 +60,10 @@ export default function Shopping() {
                                         <img src={listprodectDetials.image} className='w-100 my-3 px-1 cursor-pointer' onClick={slider} alt="" />
                                         <img src={listprodectDetials.image} className='w-100 my-3 px-1 cursor-pointer' onClick={slider} alt="" />
                                     </div>
-                                </div>
+                                </div> */}
 
                                 {/* <div className='d-flex'>
-                                {listprodectDetialsImagess.map((element, index) => {
+                                {listprodectDetials.map((element, index) => {
                                     return <> <img src={element} key={index} className='w-100 my-3 px-1 cursor-pointer' onClick={slider} alt="" />
                                     </>
                                 })}
@@ -121,7 +117,7 @@ export default function Shopping() {
                                         <button className='icon me-1 rounded-5 border-main bg-main fw-bold btn-sm'>+</button>
                                     </div>
                                     <div className='d-flex align-items-center pt-3'>
-                                        <button className='btn mx-3 my-2  rounded-5 text-white fw-bold'onClick={() => { addcart(listprodectDetials, listprodectDetials.id) }} >Add To Cart</button>
+                                        <button className='btn mx-3 my-2  rounded-5 text-white fw-bold' onClick={() => { addcart(listprodectDetials, listprodectDetials.id) }} >Add To Cart</button>
                                         <button className='btn btn1 rounded-5 fw-bold'>Pickup From Store</button>
                                     </div>
                                 </div>
